eec9ca7c91f042ef2cf20cbdc00e52938f823574
Leonardo Mariga <leomariga@gmail.com>
2020-09-03 00:09:49 -0300

    Added clustring in the local scene

--- Image_processing/aux/LocalScene.py
+++ Image_processing/aux/LocalScene.py
@@ -10,6 +10,9 @@ class LocalScene:
 
     def __init__(self, pointCloud):
         self.pointCloud = pointCloud # Total point cloud
+        self.pointCloud_notMainPlanes = []
+        self.pointCloud_objects = []
+
         self.mainPlanes = [] # List of Plane
         self.mainCylinders = [] # List of Cylinder
 
@@ -28,26 +31,39 @@ class LocalScene:
             if(qtn_inliers < 40000):
                 break
             self.mainPlanes.append(p)
+
             outlier_cloud = outlier_cloud.select_by_index(best_inliers, invert=True)
+            cl, ind = outlier_cloud.remove_radius_outlier(nb_points=500, radius=0.12)
+            #display_inlier_outlier(outlier_cloud, ind)
+            outlier_cloud = outlier_cloud.select_by_index(ind)
+        self.pointCloud_notMainPlanes = outlier_cloud
 
 
     def showMainPlanes(self):
         pointCloudList = []
         for i in range(len(self.mainPlanes)):
-            print(i)
+            #print(i)
             pcd = o3d.geometry.PointCloud()
             pcd.points = o3d.utility.Vector3dVector(self.mainPlanes[i].inliers)
-            print(self.mainPlanes[i].equation)
+            #print(self.mainPlanes[i].equation)
             pointCloudList.append(pcd.paint_uniform_color([random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)]))
         if(self.groundNormal != []):
             mesh = o3d.geometry.TriangleMesh.create_coordinate_frame(origin=[0, 0, 0], size=0.5)
             pcd = o3d.geometry.PointCloud()
             pcd.points = o3d.utility.Vector3dVector(self.mainPlanes[self.groundID].inliers)
             centerPCD = pcd.get_center()
-            print(centerPCD)
+            #print(centerPCD)
             mesh.rotate(get_rotationMatrix_from_vectors([0, 0, 1], self.groundNormal), center=(0, 0, 0)).translate(centerPCD)
             pointCloudList.append(mesh)
         o3d.visualization.draw_geometries(pointCloudList)
+
+    def showNotPlanes(self):
+        o3d.visualization.draw_geometries([self.pointCloud_notMainPlanes])
+
+    def showObjects(self):
+        o3d.visualization.draw_geometries(self.pointCloud_objects)
+
+
 
     def defineGroundNormal(self):
         normalCandidatesY = []
@@ -57,5 +73,24 @@ class LocalScene:
         idMax = normalCandidatesY.index(valMax)
         if(valMax > 0.85):
             self.groundNormal = np.asarray([self.mainPlanes[idMax].equation[0], self.mainPlanes[idMax].equation[1], self.mainPlanes[idMax].equation[2]])
+            if(self.groundNormal[1] < 0):
+                self.groundNormal = -self.groundNormal
             self.groundID = idMax
         print("Ground normal: "+str(self.groundNormal))
+
+    def clusterizeObjects(self):
+        filtered_not_planes = self.pointCloud_notMainPlanes
+        with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug) as cm:
+            labels = np.array(filtered_not_planes.cluster_dbscan(eps=0.07, min_points=200, print_progress=False))
+
+        max_label = labels.max()
+        print(f"point cloud has {max_label + 1} clusters")
+
+        o3d.visualization.draw_geometries([filtered_not_planes])
+        cluster_array = []
+        for n_cluster in range(max_label+1):
+            index_from_cluster = np.where(labels == n_cluster)[0]
+            cluster = filtered_not_planes.select_by_index( index_from_cluster.tolist())
+            cluster_qnt_points = np.asarray(cluster.points).shape[0]
+            if(cluster_qnt_points > 4000):
+                self.pointCloud_objects.append(cluster)
--- Image_processing/normal_test_ground.py
+++ Image_processing/normal_test_ground.py
@@ -33,65 +33,11 @@ for i in range(len(list_rgb)):
 	ls.findMainPlanes()
 	ls.defineGroundNormal()
 	ls.showMainPlanes()
+	ls.showNotPlanes()
+	ls.clusterizeObjects()
+	ls.showObjects()
 
 
-	## Remove radius outlier
-	# cl, ind = not_planes.remove_radius_outlier(nb_points=500, radius=0.1)
-	# filtered_not_planes = not_planes.select_by_index(ind)
-	# display_inlier_outlier(not_planes, ind)
-	# o3d.visualization.draw_geometries([filtered_not_planes])
-
-	# # Statistical oulier removal
-	# # cl, ind = not_planes.remove_statistical_outlier(nb_neighbors=1000, std_ratio=2)
-	# # #display_inlier_outlier(pcd, ind)
-	# # filtered_not_planes = not_planes.select_by_index(ind)
-	# # o3d.visualization.draw_geometries([filtered_not_planes])
-
-
-
-	# with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug) as cm:
-	# 	labels = np.array(filtered_not_planes.cluster_dbscan(eps=0.07, min_points=200, print_progress=False))
-
-	# max_label = labels.max()
-	# print(f"point cloud has {max_label + 1} clusters")
-	# colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
-	# colors[labels < 0] = 0
-	# #filtered_not_planes.colors = o3d.utility.Vector3dVector(colors[:, :3])
-	# o3d.visualization.draw_geometries([filtered_not_planes])
-	# cluster_array = []
-	# for n_cluster in range(max_label+1):
-	# 	index_from_cluster = np.where(labels == n_cluster)[0]
-
-	# 	cluster = filtered_not_planes.select_by_index( index_from_cluster.tolist())
-	# 	cluster_qnt_points = np.asarray(cluster.points).shape[0]
-	# 	if(cluster_qnt_points > 4000):
-	# 		if(showNormals):
-	# 			o3d.io.write_point_cloud("caixa6.ply", cluster)
-	# 			downpcd = cluster.voxel_down_sample(voxel_size=0.05)
-	# 			downpcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.2, max_nn=500))
-	# 			downpcd.orient_normals_towards_camera_location()
-	# 			downpcd.normalize_normals()
-
-	# 			normais = np.asarray(downpcd.normals)
-	# 			cor = np.asarray(downpcd.colors)
-
-	# 			print(cor.shape)
-
-	# 			for i in range(normais.shape[0]):
-	# 				#print(normais[i,:])
-	# 				cor[i, :] = (np.abs(normais[i,0]),np.abs(normais[i,1]),np.abs(normais[i,2]))
-
-	# 			downpcd.colors = o3d.utility.Vector3dVector(cor)
-	# 			o3d.visualization.draw_geometries([downpcd])
-
-
-	# 		cluster_array.append(cluster)
-	# 		obb = cluster.get_axis_aligned_bounding_box()
-	# 		obb2 = cluster.get_oriented_bounding_box()
-	# 		obb.color = (1,0,0)
-	# 		obb2.color = (0,1,0)
-	# 		cluster_array.append(obb)
-	# 		cluster_array.append(obb2)
 	
 	# all_objects = inlier_cloud_list[0:-1]
 	# all_objects.extend(cluster_array)
